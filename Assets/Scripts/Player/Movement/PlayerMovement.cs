c87d1f03544ff10b54b681a8b230e1a2b4067f22
dc: @himeji <tobin.post@gmail.com>
2023-10-01 21:36:29 +0200

    Improved Player Movement
    [+] Max Speed
    [+] Acceleration
    [+] Deceleration

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -7,17 +7,22 @@ public class PlayerMovement : MonoBehaviour
 {
     /* Variables */
     [Header("Object References")]
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private Transform cam;
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Transform camera;
     [SerializeField] private PlayerInput input;
 
     [Header("Camera Settings")]
     [SerializeField] private float sensitivity = 1f;
 
     [Header("Movement Settings")]
-    [SerializeField] private float speed = 1f;
-    [SerializeField] private float jumpForce = 1f;
-    [SerializeField] private float gravity = 1f;
+    [SerializeField] public static float speed = 10f;
+    [SerializeField] private float acceleration = 20f;
+    [SerializeField] private float deceleration = 40f;
+    [SerializeField] public float jumpForce = 1f;
+    [SerializeField] public float gravity = 1f;
+
+    [Header("ReadOnly Statistics")]
+    [ReadOnly][SerializeField] private float currentMoveSpeed;
 
     private Vector2 moveVector = Vector2.zero;
 
@@ -50,10 +55,33 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void FixedUpdate() {
-        Vector3 movement = new(moveVector.x, 0f, moveVector.y);
-        movement = cam.TransformDirection(movement);
+        // calculate movement
+        Vector3 movement = new Vector3(moveVector.x, 0f, moveVector.y);
+        movement = camera.TransformDirection(movement);
         movement.y = 0f;
         movement.Normalize();
-        rb.AddForce(movement * speed);
+
+        // Calculate the desired velocity based on moveVector and speed
+        //Vector3 desiredVelocity = movement * speed;
+        Vector3 currentVelocity = movement * speed;
+
+        //// Calculate the current velocity in the direction of desired velocity
+        //Vector3 currentVelocity = rb.velocity;
+        //currentVelocity.y = 0f;
+
+        //// Apply acceleration and deceleration
+        //if (desiredVelocity.magnitude > currentVelocity.magnitude) {
+        //    currentVelocity += movement * acceleration * Time.deltaTime;
+        //} else {
+        //    currentVelocity += movement * deceleration * Time.deltaTime;
+        //}
+
+        // Limit the velocity to the maximum speed (speed variable)
+        currentVelocity = Vector3.ClampMagnitude(currentVelocity, speed);
+
+        // Apply the final velocity to the Rigidbody
+        rigidbody.velocity = currentVelocity;
+
+        currentMoveSpeed = rigidbody.velocity.magnitude;
     }
 }
