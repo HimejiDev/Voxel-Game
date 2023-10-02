e10e1f0130324a46a7ccd93a6a9ead3b328d17f9
dc: @himeji <tobin.post@gmail.com>
2023-10-02 21:28:06 +0200

    Improved Movement Script
    [+] Option to disable acceleration and/or deceleration
    [+] Documentation
    [+] Moved thinks to their own function (MovePlayer(), CalculateVelocity())
    [+] Speed text on screen (m/s)
    [*] Add/Remove speed, without it being static
    [-] Player Prefab

--- Assets/Editor/DevTools.cs
+++ Assets/Editor/DevTools.cs
@@ -1,16 +1,19 @@
 using UnityEditor;
+using UnityEngine;
 
 public static class DevTools
 {
     [MenuItem("DevTools/Player/Speed (+10)")]
     public static void PlayerAdd10Speed()
     {
-        PlayerMovement.speed += 10;
+        foreach (GameObject plyr in GameObject.FindGameObjectsWithTag("Player"))
+            plyr.GetComponent<PlayerMovement>().speed += 10;
     }
 
     [MenuItem("DevTools/Player/Speed (-10)")]
     public static void PlayerRemove10Speed()
     {
-        PlayerMovement.speed -= 10;
+        foreach (GameObject plyr in GameObject.FindGameObjectsWithTag("Player"))
+            plyr.GetComponent<PlayerMovement>().speed -= 10;
     }
 }
--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     /* Variables */
     [Header("Object References")]
-    [SerializeField] private Rigidbody rigidbody;
-    [SerializeField] private Transform camera;
+    [SerializeField] private Rigidbody pRigidbody;
+    [SerializeField] private Transform pCamera;
     [SerializeField] private PlayerInput input;
+    [SerializeField] private TMP_Text speedText;
 
     [Header("Camera Settings")]
     [SerializeField] private float sensitivity = 1f;
 
     [Header("Movement Settings")]
-    [SerializeField] public static float speed = 10f;
+    [SerializeField] public float speed = 10f;
     [SerializeField] private float acceleration = 20f;
     [SerializeField] private float deceleration = 40f;
     [SerializeField] public float jumpForce = 1f;
@@ -33,6 +36,45 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnMovementCancelled(InputAction.CallbackContext context) {
         moveVector = Vector2.zero;
+    }
+
+    /// <summary>
+    /// Calculates and returns the desired velocity based on the provided movement vector and speed, considering acceleration and deceleration.
+    /// </summary>
+    /// <param name="movement">The movement vector.</param>
+    /// <returns>The calculated velocity.</returns>
+    private Vector3 CalculateVelocity(Vector3 movement) {
+        // calculate the desired velocity based on moveVector and speed
+        Vector3 desiredVelocity = movement * speed;
+
+        // calculate the current velocity in the direction of desired velocity
+        Vector3 currentVelocity = pRigidbody.velocity;
+        currentVelocity.y = pRigidbody.velocity.y;
+
+        if (desiredVelocity.magnitude > currentVelocity.magnitude) {
+            currentVelocity += (acceleration > 0f) ? acceleration * Time.deltaTime * movement : Vector3.zero;
+        } else {
+            currentVelocity += (deceleration > 0f) ? deceleration * Time.deltaTime * movement : Vector3.zero;
+        }
+
+        return (acceleration > 0f || deceleration > 0f) ? currentVelocity : desiredVelocity;
+    }
+
+    /// <summary>
+    /// Moves the player character based on the input moveVector, ensuring a maximum speed is maintained.
+    /// </summary>
+    private void MovePlayer() {
+        // calculate movement vector
+        Vector3 movement = new Vector3(moveVector.x, pRigidbody.velocity.y, moveVector.y);
+        movement = pCamera.TransformDirection(movement);
+        movement.y = pRigidbody.velocity.y;
+        movement.Normalize();
+
+        // Limit the velocity to the maximum speed (speed variable)
+        Vector3 currentVelocity = Vector3.ClampMagnitude(CalculateVelocity(movement), speed);
+
+        // Apply the final velocity to the Rigidbody
+        pRigidbody.velocity = currentVelocity;
     }
 
     /* Default Functions */
@@ -55,33 +97,10 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void FixedUpdate() {
-        // calculate movement
-        Vector3 movement = new Vector3(moveVector.x, 0f, moveVector.y);
-        movement = camera.TransformDirection(movement);
-        movement.y = 0f;
-        movement.Normalize();
+        MovePlayer();
+        
 
-        // Calculate the desired velocity based on moveVector and speed
-        //Vector3 desiredVelocity = movement * speed;
-        Vector3 currentVelocity = movement * speed;
-
-        //// Calculate the current velocity in the direction of desired velocity
-        //Vector3 currentVelocity = rb.velocity;
-        //currentVelocity.y = 0f;
-
-        //// Apply acceleration and deceleration
-        //if (desiredVelocity.magnitude > currentVelocity.magnitude) {
-        //    currentVelocity += movement * acceleration * Time.deltaTime;
-        //} else {
-        //    currentVelocity += movement * deceleration * Time.deltaTime;
-        //}
-
-        // Limit the velocity to the maximum speed (speed variable)
-        currentVelocity = Vector3.ClampMagnitude(currentVelocity, speed);
-
-        // Apply the final velocity to the Rigidbody
-        rigidbody.velocity = currentVelocity;
-
-        currentMoveSpeed = rigidbody.velocity.magnitude;
+        currentMoveSpeed = pRigidbody.velocity.magnitude;
+        speedText.text = $"Speed: {currentMoveSpeed} m/s";
     }
 }
